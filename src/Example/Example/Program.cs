e117f215a4b70aae40e7b6de701c99c177a29e9f
Hadi Fooladi Talari <hadi.f7@gmail.com>
2023-08-28 18:17:57 -0400

    Single and Two Dimensional Arrays

--- src/Example/Example/Program.cs
+++ src/Example/Example/Program.cs
@@ -6,19 +6,53 @@ internal class Program
 {
 	static void Main(string[] args)
 	{
-		Console.Write("Enter the number: ");
+		int[] numbers;
+		int[,] multi;
 
+		//int num = GetNumber();
+		//if (num == -1)
+		//{
+		//	return;
+		//}
+
+		int rows = GetNumber(), cols = GetNumber();
+
+		if (rows == -1 || cols == -1) return;
+
+		multi = new int[rows, cols];
+
+		for(int row = 0; row < rows;row++)
+		for (int col = 0; col < cols; col++)
+			multi[row, col] = row * col;
+
+		//numbers = new int[num];
+
+		//for (int ndx = 0; ndx < num; ndx++)
+		//	numbers[ndx] = ndx;
+
+		//foreach (var number in numbers)
+		//{
+		//	Console.WriteLine(number);
+		//}
+
+		return;
+
+		//Console.Write("Enter the number: ");
+
+
+		//for (int i = 1; i <= num; i++)
+		//{
+		//	Console.WriteLine(i);
+		//}
+	}
+
+	static int GetNumber()
+	{
 		var response = Console.ReadLine();
 
-		if (!int.TryParse(response, out var num))
-		{
-			Console.WriteLine("Invliad Number!");
-			return;
-		}
+		if (int.TryParse(response, out var num))
+			return num;
 
-		for (int i = 1; i <= num; i++)
-		{
-			Console.WriteLine(i);
-		}
+		return -1;
 	}
 }
