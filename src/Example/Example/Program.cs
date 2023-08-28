8ea26e68a1a05420f397f1af16aa3dc6216e5927
Hadi Fooladi Talari <hadi.f7@gmail.com>
2023-08-28 18:02:29 -0400

    Update Program.cs

--- src/Example/Example/Program.cs
+++ src/Example/Example/Program.cs
@@ -6,6 +6,19 @@ internal class Program
 {
 	static void Main(string[] args)
 	{
-		Console.WriteLine("Hello, World!");
+		Console.Write("Enter the number: ");
+
+		var response = Console.ReadLine();
+
+		if (!int.TryParse(response, out var num))
+		{
+			Console.WriteLine("Invliad Number!");
+			return;
+		}
+
+		for (int i = 1; i <= num; i++)
+		{
+			Console.WriteLine(i);
+		}
 	}
 }
