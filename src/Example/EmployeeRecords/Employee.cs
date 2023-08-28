eb66b7329fec6cd941445df471966cb83cf75271
Hadi Fooladi Talari <hadi.f7@gmail.com>
2023-08-28 18:53:29 -0400

    New Console Project: EmployeeRecords

---
+++ src/Example/EmployeeRecords/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRecords;
+
+class Employee
+{
+	internal string FirstName { get; set; }
+	public string LastName { get; set; }
+
+	public DateTime BirthDate { get; set; }
+
+	//private string _firstName;
+
+	//public string FirstName
+	//{
+	//	get => _firstName;
+	//	set => _firstName = value;
+	//}
+
+	public string FullName
+	{
+		//get => FirstName + " " + LastName;
+		get => $"{FirstName} {LastName}";
+	}
+
+	public string FullName2 => $"{FirstName} {LastName}";
+
+	public int Age => (int)((DateTime.Now - BirthDate).TotalDays / 365);
+
+	public override string ToString()
+	{
+		return $"{FullName} (Age: {Age})";
+	}
+}
---
+++ src/Example/EmployeeRecords/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EmployeeRecords
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//var employee = new Employee();
+			//Employee employee = new Employee();
+			Employee employee = new()
+			{
+				FirstName = "John",
+				LastName = "Doe",
+				BirthDate = new DateTime(1956, 4, 23)
+			};
+
+			Console.WriteLine(employee);
+		}
+	}
+}
