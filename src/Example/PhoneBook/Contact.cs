711baebd3b75534a233f66a77529f8467295e806
Hadi Fooladi Talari <hadi.f7@gmail.com>
2023-09-18 18:22:29 -0400

    PhoneBook (v0.0.4) [File]

--- src/Example/PhoneBook/Contact.cs
+++ src/Example/PhoneBook/Contact.cs
@@ -4,6 +4,13 @@ namespace PhoneBook;
 
 class Contact
 {
+	public static Contact From(string packValue)
+	{
+		var contact = new Contact();
+		contact.Unpack(packValue);
+		return contact;
+	}
+
 	public string FirstName { get; set; }
 	public string LastName { get; set; }
 	public string Email { get; set; }
@@ -28,5 +35,19 @@ class Contact
 	public override string ToString()
 	{
 		return $"{FullName} (Email: {Email}) (Phone: {PhoneNumber})";
+	}
+
+	public string Pack()
+	{
+		return string.Join("$%", FirstName, LastName, Email, PhoneNumber);
+	}
+
+	public void Unpack(string s)
+	{
+		var sp = s.Split("$%");
+		FirstName = sp[0];
+		LastName = sp[1];
+		Email = sp[2];
+		PhoneNumber = sp[3];
 	}
 }
--- src/Example/PhoneBook/Program.cs
+++ src/Example/PhoneBook/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace PhoneBook;
 
@@ -8,12 +9,16 @@ internal class Program
 {
 	static void Main()
 	{
+		const string DATA_FILENAME = "Data.txt";
+
 		var contacts = new List<Contact>();
 
 		string[] options =
 		{
 			"New Contact",
 			"Write Contacts",
+			"Save",
+			"Load",
 			"Exit"
 		};
 
@@ -45,6 +50,22 @@ internal class Program
 					break;
 
 				case 3:
+					File.WriteAllLines(DATA_FILENAME, contacts.Select(c => c.Pack()));
+					break;
+
+				case 4:
+					if (!File.Exists(DATA_FILENAME))
+					{
+						Console.WriteLine($"{DATA_FILENAME} doesn't exist");
+						break;
+					}
+
+					contacts = File.ReadAllLines(DATA_FILENAME)
+						.Select(Contact.From)
+						.ToList();
+					break;
+
+				case 5:
 					return;
 			}
 		}
