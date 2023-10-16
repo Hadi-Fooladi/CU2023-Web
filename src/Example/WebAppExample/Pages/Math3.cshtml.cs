fa1873ce44f3bfc3a15518ed0544148bdbd799fc
Hadi Fooladi Talari <hadi.f7@gmail.com>
2023-10-16 19:12:18 -0400

    URL generation by Url.Page method

--- src/Example/WebAppExample/Pages/Math3.cshtml.cs
+++ src/Example/WebAppExample/Pages/Math3.cshtml.cs
@@ -10,21 +10,22 @@ namespace WebAppExample.Pages
 
         public string Text { get; set; }
 
+        public int Result { get; set; }
+
         public void OnGet()
         {
             int num1 = Input.Num1;
             int num2 = Input.Num2;
             string s = Input.Op;
 
-            int result = 0;
             char op = '?';
 
             switch (s)
             {
-                case "add": result = num1 + num2; op = '+'; break;
-                case "sub": result = num1 - num2; op = '-'; break;
-                case "mul": result = num1 * num2; op = '*'; break;
-                case "div": result = num1 / num2; op = '/'; break;
+                case "add": Result = num1 + num2; op = '+'; break;
+                case "sub": Result = num1 - num2; op = '-'; break;
+                case "mul": Result = num1 * num2; op = '*'; break;
+                case "div": Result = num1 / num2; op = '/'; break;
             }
 
             if (op == '?')
@@ -33,7 +34,7 @@ namespace WebAppExample.Pages
             }
             else
             {
-                Text = $"{num1} {op} {num2} = {result}";
+                Text = $"{num1} {op} {num2} = {Result}";
             }
         }
     }
