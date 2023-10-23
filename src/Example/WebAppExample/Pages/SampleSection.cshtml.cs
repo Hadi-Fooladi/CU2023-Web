7d26e09b47c1d7bcec1f5382defdc546c8f2ae6e
Hadi Fooladi Talari <hadi.f7@gmail.com>
2023-10-23 18:12:47 -0400

    Layout with sections example

---
+++ src/Example/WebAppExample/Pages/SampleSection.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppExample.Pages
+{
+    public class SampleSectionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
