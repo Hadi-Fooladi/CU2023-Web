f92316d64b07c9d0c0d9d2ee8b18928d047e6d72
Hadi Fooladi Talari <hadi.f7@gmail.com>
2023-10-30 19:16:31 -0400

    Showing contacts

---
+++ src/Example/PhoneBookWeb/Classes/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBookWeb;
+
+public class Contact
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
---
+++ src/Example/PhoneBookWeb/Classes/Global.cs
@@ -0,0 +1,17 @@
+﻿using PhoneBookWeb;
+
+namespace PhoneBookWebs;
+
+static class Global
+{
+    public static List<Contact> Contacts = new()
+    {
+        new Contact()
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            PhoneNumber = "123-456-7890",
+            Email = "JohnDoe@gmail.com"
+        }
+    };
+}
