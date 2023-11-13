fbc133ea7e29999cfc5d18dc3c790d88735af03c
Hadi Fooladi Talari <hadi.f7@gmail.com>
2023-11-13 17:49:58 -0500

    Getting input using a form

--- src/Example/PhoneBookWeb/Classes/Contact.cs
+++ src/Example/PhoneBookWeb/Classes/Contact.cs
@@ -1,9 +1,18 @@
-﻿namespace PhoneBookWeb;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBookWeb;
 
 public class Contact
 {
+    [Display(Name = "First Name:")]
     public string FirstName { get; set; }
+
+    [Display(Name = "Last Name:")]
     public string LastName { get; set; }
+    
+    [Display(Name = "Email:")]
     public string Email { get; set; }
+    
+    [Display(Name = "Phone #:")]
     public string PhoneNumber { get; set; }
 }
---
+++ src/Example/PhoneBookWeb/Pages/Add.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhoneBookWeb.Pages
+{
+    public class AddModel : PageModel
+    {
+		[BindProperty]
+        public Contact Contact { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            Global.Contacts.Add(Contact);
+
+            return RedirectToPage("Index");
+        }
+    }
+}
