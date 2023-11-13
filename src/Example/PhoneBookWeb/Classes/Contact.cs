0681fe993c55b46007c4763d4ee0cbfe36a18c42
Hadi Fooladi Talari <hadi.f7@gmail.com>
2023-11-13 18:26:12 -0500

    Dropdownlist for Contact.Gender

--- src/Example/PhoneBookWeb/Classes/Contact.cs
+++ src/Example/PhoneBookWeb/Classes/Contact.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace PhoneBookWeb;
 
@@ -15,4 +16,14 @@ public class Contact
     
     [Display(Name = "Phone #:")]
     public string PhoneNumber { get; set; }
+
+    [Display(Name = "Gender:")]
+    public string Gender { get; set; }
+
+    public static readonly List<SelectListItem> Genders = new()
+    {
+        new() { Value = "M", Text = "Male" },
+        new() { Value = "F", Text = "Female" },
+        new() { Value = "O", Text = "Other" },
+    };
 }
