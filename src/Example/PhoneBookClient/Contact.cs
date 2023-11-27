5471b528305914c5f760884261e697cdfbba0ae2
Hadi Fooladi Talari <hadi.f7@gmail.com>
2023-11-27 17:34:51 -0500

    New project: PhoneBookClient (Used to consume the web API)

---
+++ src/Example/PhoneBookClient/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBookClient;
+
+public class Contact
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string PhoneNumber { get; set; }
+
+    public string Gender { get; set; }
+}
---
+++ src/Example/PhoneBookClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace PhoneBookClient
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Test();
+
+			Thread.Sleep(5000);
+
+			Console.Write("\n\nPress any key to exit...");
+			Console.ReadKey();
+		}
+
+		static async void Test()
+		{
+			var client = new HttpClient
+			{
+				BaseAddress = new Uri("https://localhost:44338/")
+			};
+
+			var response = await client.GetStringAsync("api/Main/GetContacts");
+
+			Console.WriteLine(response);
+
+			int row = 1;
+			var contacts = JsonConvert.DeserializeObject<List<Contact>>(response);
+			foreach (var contact in contacts)
+			{
+				Console.WriteLine($"{row++}- {contact.FirstName} {contact.LastName}");
+			}
+		}
+	}
+}
